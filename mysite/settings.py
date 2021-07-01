708c22f4bf3224fe8ba7ae42d45ef9f36d8f96ff
marquez <marknovak85@gmail.com>
2021-07-01 17:54:51 +0200

    added static folder - tut part6

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's6t9+ceb@#1t-4e2t*dc^$88r7mbnygg%vthj9my148u80nvij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
